342eec28f52838769ef7b9c853a6b1d561ab5ffa
ALEJORIOS <luisrios.lar@gmail.com>
2021-01-09 19:01:36 -0500

    added package features
    Second release

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools import find_packages
+setup(
+
+    name = "vect",
+    version = "0.2",
+    description = "This powerful tool can help you in vector related work in python",
+    author = "Alejandro Ríos",
+    keywords = ["vect", "vector", "algebra"],
+    packages = ["vect","vect"],
+)
