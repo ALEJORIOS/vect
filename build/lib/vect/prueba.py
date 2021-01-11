5030cea27df0b47610ba470d50ef94553ea8bf9d
ALEJORIOS <luisrios.lar@gmail.com>
2021-01-11 01:48:56 -0500

    Delete trash

--- build/lib/vect/prueba.py
+++ build/lib/vect/prueba.py
@@ -3,7 +3,3 @@ v = vector.array([[1,-1,1],[2,2,3],[-2,-3,-1]])
 v2 = vector.array([[1,0,4],[0,2,5],[1,3,0]])
 
 print(v@v2)
-
-# v = vector.array([[1,2,3],[4,5,6]])
-# v2 = vector.array([[1],[2],[3]])
-# print(v@v2)
--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open("README.md", "r") as readme_file:
 setup(
 
     name = "vect",
-    version = "0.0.4",
+    version = "0.0.5b",
     description = "This powerful tool can help you in vector related work in python",
     author = "Alejandro Ríos",
     long_description = readme,
     url = "https://github.com/ALEJORIOS/vect.git",
     keywords = ["vect", "vector", "algebra"],
-    packages = ["vect","vector"],
+    packages = ["vect","vect"],
 )
